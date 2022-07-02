f76566da2825561a14221bffe852ec70d3becd0d
VladSmaug <incrediblesmaug@gmail.com>
2022-07-02 21:37:43 +0300

    Browser Router wrapping unsuccessful

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
 import Dialogs from "./components/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header nameLogoDefault="Logo1" nameLogoSample="Logo2" />
-      <Nav guide="This is nav" />
-      <div className="app-wrapper-content">
-        <Profile />
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header nameLogoDefault="Logo1" nameLogoSample="Logo2" />
+        <Nav guide="This is nav" />
+        <div className="app-wrapper-content">
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
