bb664423ba5a1719ddae650218e7e841de82c442
VladSmaug <107713492+VladSmaug@users.noreply.github.com>
2022-07-02 16:27:45 +0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import f from "./Header.module.css";
 
-const Header = (objectProps) => {
+const Header = (props) => {
   return (
     <header className={f.header}>
       <img
         src="https://cdn.logo.com/hotlink-ok/logo-social.png"
         alt="Picture"
       />
-      {objectProps.nameLogoDefault}
+      {props.nameLogoDefault}
       <img
         src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
         alt="Picture"
       />
-      {objectProps.nameLogoSample}
+      {props.nameLogoSample}
     </header>
   );
 };
