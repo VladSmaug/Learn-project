cdb0fa05f96117da24171eb6e8c7f3075f9528a4
VladSmaug <incrediblesmaug@gmail.com>
2022-07-04 13:29:36 +0300

    Router changed with corrections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import "./App.css";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Profile from "./components/Profile";
-import Dialogs from "./components/Dialogs";
 import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Nav from "./components/Nav";
+
+import Profile from "./components/Profile";
+
+import Dialogs from "./components/Dialogs";
+
+import "./App.css";
+
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Nav />
+
       <Routes>
         <Route path="profile" element={<Profile />} />
         <Route path="dialogs" element={<Dialogs />} />
