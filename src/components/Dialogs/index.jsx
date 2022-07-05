35dfc6c180d466144724ce8d51b417d0f8d6c35d
VladSmaug <107713492+VladSmaug@users.noreply.github.com>
2022-07-05 15:38:49 +0300

    Added dialog window with stylization and content (#8)

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import classNames from "classnames";
 
 import styles from "./index.module.css";
 
 const Dialogs = () => {
-  return <div className={styles.item}>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={classNames(styles.dialog, styles.active)}>Vlad</div>
+        <div className={styles.dialog}>FFFF</div>
+        <div className={styles.dialog}>Username1</div>
+        <div className={styles.dialog}>Username2</div>
+        <div className={styles.dialog}>Username3</div>
+        <div className={styles.dialog}>Username4</div>
+        <div className={styles.dialog}>Username5</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={styles.message}>Lorem ipsum dolor sit amet.</div>
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
