f7995f97280206c080f63fbb47e3cf12b37bb67d
VladSmaug <107713492+VladSmaug@users.noreply.github.com>
2022-07-05 10:04:13 +0300

    Beggining Lesson 20 (#5)

--- src/components/Profile/My posts/MyPost.jsx
+++ src/components/Profile/My posts/MyPost.jsx
@@ -5,6 +5,7 @@ const MyPost = () => (
     My posts
     <Post message="Hi how are you" likes="10" />;
     <Post message="Hello how are you?" likes="20" />;
+    <div>New for Lesson 20</div>
   </div>
 );
 
