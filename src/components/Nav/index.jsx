87cb13333af8f0be138a6ed2c97d1ac11a746270
VladSmaug <incrediblesmaug@gmail.com>
2022-07-05 12:09:03 +0300

    With Active NavLink

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from "./Nav.module.css";
 
@@ -8,9 +8,13 @@ const Nav = (props) => {
   const { guide } = props;
   return (
     <nav className={styles.nav}>
-      <ul>
-        <Link to="/profile">Profile</Link>
-        <Link to="/dialogs">Dialogs</Link>
+      <ul className={styles.item}>
+        <NavLink to="/profile" className={styles.activeLink}>
+          Profile
+        </NavLink>
+        <NavLink to="/dialogs" className={styles.activeLink}>
+          Dialogs
+        </NavLink>
         <li>
           <a href="#">About</a>
         </li>
