06e6584baca19ec430ecf763f4bd59e64ec17880
VladSmaug <107713492+VladSmaug@users.noreply.github.com>
2022-07-08 17:46:38 +0300

    With map (#13)

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 
+import TableReg from "../MAP";
+
 import styles from "./index.module.css";
+
+const USERS_LIST = [
+  {
+    username: "Vlad",
+    surname: "H",
+    age: "25",
+  },
+  {
+    username: "Vlad",
+    surname: "H",
+    age: "25",
+  },
+  {
+    username: "Vlad",
+    surname: "H",
+    age: "25",
+  },
+  {
+    username: "Vlad",
+    surname: "H",
+    age: "25",
+  },
+];
 
 const dialogsData = [
   { id: 1, name: "Vlad" },
@@ -31,18 +56,19 @@ const Dialogs = () => {
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>
         <DialogItem
-          name={dialogsData.map((dia) => (
-            <li>{dia.name}</li>
+          name={dialogsData.map((dia, index) => (
+            <li key={index}>{dia.name}</li>
           ))}
         />
       </div>
       <div className={styles.messages}>
         <MessageItem
-          text={messagesData.map((message) => (
-            <li>{message.text}</li>
+          text={messagesData.map((message, index) => (
+            <li key={index}>{message.text}</li>
           ))}
         />
       </div>
+      <TableReg usersList={USERS_LIST} />
     </div>
   );
 };
---
+++ src/components/MAP/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import style from "./MAP.module.css";
+
+const TableTitle = (props) => {
+  const { username, surname, age } = props;
+  return (
+    <thead>
+      <tr className={style.table_title}>
+        <td>{username}</td>
+        <td>{surname}</td>
+        <td>{age}</td>
+      </tr>
+    </thead>
+  );
+};
+
+const TableReg = ({ usersList }) => {
+  return (
+    <table className={style.table}>
+      <TableTitle username="Ім'я" surname="Прізвище" age="Вік" />
+      <tbody>
+        <tr className={style.table_rows}>
+          {usersList.map((user, index) => (
+            <React.Fragment key={index}>
+              <td className={style.name_of_user}>{user.username}</td>
+              <td className={style.surname_of_user}>{user.surname}</td>
+              <td className={style.age_of_user}>{user.age}</td>
+            </React.Fragment>
+          ))}
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableReg;
