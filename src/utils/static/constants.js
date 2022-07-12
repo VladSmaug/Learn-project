ab5a80d8f95056d548357120d773a8caad25a4f6
VladSmaug <incrediblesmaug@gmail.com>
2022-07-12 16:40:03 +0300

    New object added

--- src/utils/static/constants.js
+++ src/utils/static/constants.js
@@ -25,3 +25,23 @@ export const POST_DATA = [
   { message: "Hi how are you", likes: 10, id: 1 },
   { message: "Hello how are you?", likes: 20, id: 2 },
 ];
+
+export const USERS = [
+  {
+    avatar: <img src="./images/ava1.png" alt="avatar" />,
+    text: "lorem",
+  },
+  {
+    avatar: <img src="" alt="avatar" />,
+    text: "lorem",
+  },
+  {
+    avatar: (
+      <img
+        src="src\images\pngtree-human-avatar-free-vector-png-image_4825373.png"
+        alt="avatar"
+      />
+    ),
+    text: "lorem",
+  },
+];
