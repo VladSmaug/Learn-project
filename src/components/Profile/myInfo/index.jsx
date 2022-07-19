bd335d83f76e9e27cd367d73d01c9d59d428ebcf
VladSmaug <incrediblesmaug@gmail.com>
2022-07-19 16:59:48 +0300

    css changed

--- src/components/Profile/myInfo/index.jsx
+++ src/components/Profile/myInfo/index.jsx
@@ -5,13 +5,16 @@ import styles from "./MyInfo.module.css";
 const MyInfo = () => {
   return (
     <div>
-      <div>
+      <div className={styles.wrapper}>
         <img
           src="https://img.freepik.com/free-photo/puppy-labrador-retriever_87557-10551.jpg?w=2000"
           alt="Avatar-sdsd"
           className={styles.avatar}
         />
-        <div>avatar + description</div>
+        <div className={styles.text}>
+          <div>username</div>
+          <div>userstatus</div>
+        </div>
       </div>
     </div>
   );
