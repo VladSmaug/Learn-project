3c641e533f08243368d983bec79eadf05130d57d
VladSmaug <107713492+VladSmaug@users.noreply.github.com>
2022-07-20 14:28:05 +0300

    Lesson 35 (#20)

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,79 +1,87 @@
-import { rerenderEntireTree } from "../render";
-
-export const state = {
-  DIALOGS: {
-    USERS_LIST: [
-      {
-        username: "Vlad",
-        surname: "H",
-        age: "25",
-      },
-      {
-        username: "Big Smoke",
-        surname: "Dog",
-        age: "31",
-      },
-      {
-        username: "C",
-        surname: "J",
-        age: "28",
-      },
-      {
-        username: "Sweet",
-        surname: "308",
-        age: "30",
-      },
-    ],
+let store = {
+  _state: {
+    DIALOGS: {
+      USERS_LIST: [
+        {
+          username: "Vlad",
+          surname: "H",
+          age: "25",
+        },
+        {
+          username: "Big Smoke",
+          surname: "Dog",
+          age: "31",
+        },
+        {
+          username: "C",
+          surname: "J",
+          age: "28",
+        },
+        {
+          username: "Sweet",
+          surname: "308",
+          age: "30",
+        },
+      ],
+    },
+    MESSAGES: {
+      POST_DATA: [
+        { message: "Hi how are you", likes: 10, id: 1 },
+        { message: "Hello how are you?", likes: 20, id: 2 },
+      ],
+      NEW_POST_TEXT: "asd",
+    },
+    NAV_FRIENDS: {
+      USERS_DATA: [
+        {
+          avatar: <img src="\images\ava1.png" alt="avatar" />,
+          text: "lorem",
+        },
+        {
+          avatar: (
+            <img
+              src="\images\man-avatar-vector-silhouettes-of-people-132032.png"
+              alt="avatar"
+            />
+          ),
+          text: "lorem",
+        },
+        {
+          avatar: (
+            <img
+              src="\images\pngtree-human-avatar-free-vector-png-image_4825373.png"
+              alt="avatar"
+            />
+          ),
+          text: "lorem",
+        },
+      ],
+    },
   },
-  MESSAGES: {
-    POST_DATA: [
-      { message: "Hi how are you", likes: 10, id: 1 },
-      { message: "Hello how are you?", likes: 20, id: 2 },
-    ],
-    NEW_POST_TEXT: "asd",
+  getState() {
+    return this._state;
   },
-  NAV_FRIENDS: {
-    USERS_DATA: [
-      {
-        avatar: <img src="\images\ava1.png" alt="avatar" />,
-        text: "lorem",
-      },
-      {
-        avatar: (
-          <img
-            src="\images\man-avatar-vector-silhouettes-of-people-132032.png"
-            alt="avatar"
-          />
-        ),
-        text: "lorem",
-      },
-      {
-        avatar: (
-          <img
-            src="\images\pngtree-human-avatar-free-vector-png-image_4825373.png"
-            alt="avatar"
-          />
-        ),
-        text: "lorem",
-      },
-    ],
+  _callSubscriber() {
+    console.log("state");
+  },
+  addPost() {
+    const newPost = {
+      message: this._state.MESSAGES.NEW_POST_TEXT,
+      likes: 0,
+      id: 1,
+    };
+    this._state.MESSAGES.POST_DATA.push(newPost);
+    this._state.MESSAGES.NEW_POST_TEXT = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newPostText) {
+    this._state.MESSAGES.NEW_POST_TEXT = newPostText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export const addPost = () => {
-  const newPost = {
-    message: state.MESSAGES.NEW_POST_TEXT,
-    likes: 0,
-    id: 1,
-  };
-  state.MESSAGES.POST_DATA.push(newPost);
-  state.MESSAGES.NEW_POST_TEXT = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newPostText) => {
-  state.MESSAGES.NEW_POST_TEXT = newPostText;
-  rerenderEntireTree(state);
-};
-
-export default state;
+export default store;
+window.store = store;
