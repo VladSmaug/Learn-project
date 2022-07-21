e7aa960d05dd270ec95a5602a463ab6d7fe2640f
VladSmaug <incrediblesmaug@gmail.com>
2022-07-21 16:43:26 +0300

    Some css editions in messages page

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -41,12 +41,21 @@ const Dialogs = ({ list }) => {
       <div className={styles.dialogsItems}>
         <DialogItem
           name={store._state.CONVERSATIONS.map((dia, index) => (
-            <li key={index}>{dia.name}</li>
+            <li className={styles.link} key={index}>
+              <img
+                className={styles.image}
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/ea7a3c32163929.567197ac70bda.png"
+                alt=""
+              />
+              {dia.name}
+            </li>
           ))}
         />
         <MessageItem
           text={store._state.CONVERSATION_MESSAGES.map((message, index) => (
-            <li key={index}>{message.text}</li>
+            <li className={styles.texts} key={index}>
+              {message.text}
+            </li>
           ))}
         />
       </div>
