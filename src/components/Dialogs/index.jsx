0cfc377288b17e6f784c17733df46b6ce1fd0979
VladSmaug <incrediblesmaug@gmail.com>
2022-07-21 15:16:29 +0300

    with some message editions

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -3,16 +3,24 @@ import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 
 import styles from "./index.module.css";
-import TableReg from "../MAP";
+// import TableReg from "../MAP";
 
 const dialogsData = [
   { id: 1, name: "Vlad" },
-  { id: 2, name: "Vlad" },
+  { id: 2, name: "Somebody else" },
+  { id: 3, name: "Somebody else" },
+  { id: 4, name: "Somebody else" },
+  { id: 5, name: "Somebody else" },
+  { id: 6, name: "Somebody else" },
 ];
 
 const messagesData = [
   { text: "lorem", id: 1 },
   { text: "lorem", id: 2 },
+  { text: "lorem", id: 3 },
+  { text: "lorem", id: 4 },
+  { text: "lorem", id: 5 },
+  { text: "lorem", id: 6 },
 ];
 
 const DialogItem = (props) => {
@@ -29,16 +37,16 @@ const MessageItem = (props) => {
   return <div className={styles.message}>{text}</div>;
 };
 
-const areaRef = createRef();
+// const areaRef = createRef();
 
-const onClickHandler = () => {
-  const areaValue = areaRef.current?.value;
-  alert(areaValue);
-};
+// const onClickHandler = () => {
+//   const areaValue = areaRef.current?.value;
+//   alert(areaValue);
+// };
 
-const onChangeHandler = (event) => {
-  console.log(event.target.value);
-};
+// const onChangeHandler = (event) => {
+//   console.log(event.target.value);
+// };
 
 const Dialogs = ({ list }) => {
   return (
@@ -49,18 +57,17 @@ const Dialogs = ({ list }) => {
             <li key={index}>{dia.name}</li>
           ))}
         />
-      </div>
-      <div className={styles.messages}>
         <MessageItem
           text={messagesData.map((message, index) => (
             <li key={index}>{message.text}</li>
           ))}
         />
       </div>
-      <TableReg usersList={list} />
+
+      {/*  <TableReg usersList={list} />
       <textarea ref={areaRef} />
       <button onClick={onClickHandler}>Submit</button>{" "}
-      <input type="text" onChange={onChangeHandler} />
+      <input type="text" onChange={onChangeHandler} /> */}
     </div>
   );
 };
